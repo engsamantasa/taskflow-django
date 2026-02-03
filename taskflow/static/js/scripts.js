ebaa1e552a4ce0a9446598ee5a54b4091457bd00
Samanta Sá <saksamantha@gmail.com>
2026-02-03 14:11:31 -0300

    Criando rotas e o HTML base

---
+++ taskflow/static/js/scripts.js
@@ -0,0 +1,47 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    
+    
+    
+    // Sumir alertas do Django após 4 segundos
+    const alerts = document.querySelectorAll('.alert');
+    if (alerts) {
+        setTimeout(() => {
+            alerts.forEach(alert => {
+                // Adiciona classe de fade out do Bootstrap
+                alert.classList.remove('show');
+                alert.classList.add('fade');
+                // Remove do DOM após a animação
+                setTimeout(() => alert.remove(), 500);
+            });
+        }, 4000); // 4 segundos
+    }
+
+    
+    
+    
+    
+    
+    // 2. Confirmação antes de deletar 
+    const deleteButtons = document.querySelectorAll('.btn-delete');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const confirmacao = confirm("Tem certeza que deseja excluir esta tarefa? Esta ação não pode ser desfeita.");
+            if (!confirmacao) {
+                event.preventDefault(); // Cancela o clique se o usuário disser "Cancelar"
+            }
+        });
+    });
+
+
+
+
+
+
+    // 3. Inicializar Tooltips do Bootstrap (caso eu use)
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+});
