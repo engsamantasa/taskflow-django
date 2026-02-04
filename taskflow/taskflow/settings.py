bf03834ea96e772e8b448b2483b726051ef4d46f
Samanta Sá <saksamantha@gmail.com>
2026-02-04 17:00:45 -0300

    Primeiro migrate e começo de personalização

--- taskflow/taskflow/settings.py
+++ taskflow/taskflow/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'taskflow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
