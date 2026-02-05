92456b01b3e5ce279c410a27498639acb3273649
Samanta Sá <saksamantha@gmail.com>
2026-02-05 11:53:07 -0300

    Apagando código não utilizado

--- taskflow/accounts/urls.py
+++ taskflow/accounts/urls.py
@@ -3,13 +3,8 @@ from django.contrib.auth import views as auth_views # Importamos as views pronta
 from . import views
 
 urlpatterns = [
-    # Rota Raiz (http://localhost:8000/)
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/auth.html'), name='login'),
-    
-    # Logout (redireciona para a home após sair)
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
-    # Cadastro
     path('register/', views.register, name='register'),
 ]
--- taskflow/accounts/views.py
+++ taskflow/accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from .forms import CadastroUsuarioForm
 
 
@@ -12,15 +11,12 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        # Preenche o formulário com os dados que vieram do HTML
         form = CadastroUsuarioForm(request.POST)
         
         if form.is_valid():
-            user = form.save() # Salva no banco de dados
-            login(request, user) # Faz o login automático após cadastrar
-            return redirect('task_list') # Manda para o dashboard
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
     else:
         form = CadastroUsuarioForm()
-    
-    # Se der erro ou for a primeira visita, renderiza a página com o form (e erros se houver)
     return render(request, 'registration/auth.html', {'form': form})
