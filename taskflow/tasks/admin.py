8463cffb40047186897f98ccfb2046921fd5f238
Samanta Sá <saksamantha@gmail.com>
2026-02-05 17:43:18 -0300

    Salvando CRUD pelo site

--- taskflow/tasks/admin.py
+++ taskflow/tasks/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Task
 
 
@@ -7,7 +8,7 @@ class TaskAdmin(admin.ModelAdmin):
 
     list_display = (
         'title',
-        'status',
+        'colored_status',
         'due_date',
         'user',
         'created_at',
@@ -53,3 +54,22 @@ class TaskAdmin(admin.ModelAdmin):
             )
         }),
     )
+
+    def colored_status(self, obj):
+        if obj.status == Task.STATUS_PENDING:
+            color = 'orange'
+            label = 'Pendente'
+        elif obj.status == Task.STATUS_IN_PROGRESS:
+            color = 'blue'
+            label = 'Em Andamento'
+        else:
+            color = 'green'
+            label = 'Completado'
+
+        return format_html(
+            '<strong style="color: {};">{}</strong>',
+            color,
+            label
+        )
+
+    colored_status.short_description = 'Status'
--- taskflow/tasks/urls.py
+++ taskflow/tasks/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('create/', views.task_create, name='task_create'),
+    path('<int:pk>/edit/', views.task_update, name='task_update'),
+    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
